0f24bb0bc5eab8ab2d34dd952ced4b6c4b14e691
Dan Kincaid <dankincaid@Dans-MacBook-Pro.local>
2018-02-05 12:43:16 -0800

    Change Request Time

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Locations Array of Zip Codes
 $locations = ['90210', '37901', '77073', '02035'];
 // Minutes between requests to the API
-$requestInterval = 1;
+$requestInterval = 60;
 // Sets how long you want the script to run in seconds
 // This is not actual time, just time spent executing the script
 // 0 will make the script run endlessly
